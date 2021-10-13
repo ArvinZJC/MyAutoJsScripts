644cf84e2717883b960e812ff752cccb39930177
ArvinZJC <tomzjc@qq.com>
2021-10-13 22:48:54 +0100

    feat: release SMS Forwarder V1.0.0-beta
    1. Improve the logic of detecting SMS messages received.
    2. Add a simple settings storage to enable configuring Telegram user ID.
    3. Enable a foreground service to keep the app alive.
    4. Improve the Telegram bot message template.
    5. Redesign UI.
    6. Enable showing logs in a floating window (dev mode).

--- SmsForwarder/main.js
+++ SmsForwarder/main.js
@@ -1,19 +1,40 @@
 /*
  * @Description: the core of SMS Forwarder
- * @Version: 1.0.0.20211003
+ * @Version: 1.1.0.20211013
  * @Author: Arvin Zhao
  * @Date: 2021-10-03 15:10:01
  * @Last Editors: Arvin Zhao
- * @LastEditTime: 2021-10-03 19:07:56
+ * @LastEditTime: 2021-10-13 22:42:59
  */
 
-"ui";
+"ui"; // Enable the UI mode of Auto.js Pro.
 
+importClass(android.content.ContentValues);
 importClass(android.database.ContentObserver);
 importClass(android.net.Uri);
 
-const BOT_TOKEN = "2005164021:AAFFGEy5aK_L8xGgBl_g-CAzEFCtOq02Du0"; // The corresponding Telegram bot token.
+const CONFIG = require("./config.json");
+const GONE = 8
+const HIDE_LOGS = "Hide logs"
+const SHOW_LOGS = "Show logs"
+const VISIBLE = 0
+var storage = storages.create("ArvinZJC:SmsForwarder")
 var smsObserver = null;
+
+/**
+ * Show and change settings if applicable.
+ */
+function changeSettings() {
+    var tgUserId = storage.get("tgUserId");
+    rawInput("Enter your Telegram user ID", tgUserId).then(input => {
+        input = input.trim()
+
+        if (tgUserId !== input) {
+            storage.put("tgUserId", input);
+            log("TG user ID configured.")
+        } // end if
+    });
+} // end function changeSettings
 
 /**
  * End the SMS observer.
@@ -30,29 +51,34 @@ function endObserver() {
  * @param {String} msg a string containing the sender and the content of the new SMS message
  */
 function forwardSms(msg) {
-    var tgUserId = String(ui.tgUserId.getText()).trim()
+    if (storage.contains("tgUserId")) {
+        var tgUserId = storage.get("tgUserId");
+
+        if (tgUserId === "") {
+            log("Invalid TG user ID.")
+        }
+        else {
+            threads.start(function() {
+                try {
+                    var r = http.get("https://api.telegram.org/bot" + CONFIG.tgBotToken + "/sendMessage?chat_id=" + tgUserId + "&parse_mode=HTML&text=" + msg);
     
-    if (tgUserId !== null) {
-        threads.start(function() {
-            try {
-                var r = http.get("https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage?chat_id=" + tgUserId + "&text=" + msg);
-
-                if (r.statusCode === 200) {
-                    toastLog("New SMS message forwarded to TG.");
+                    if (r.statusCode === 200) {
+                        toastLog("New SMS message forwarded to TG.");
+                    }
+                    else {
+                        log(r);
+                    } // end if...else
                 }
-                else {
-                    log(r);
-                } // end if...else
-            }
-            catch (e) {
-                log("Failed to forward the new SMS message.");
-                log(e);
-            } // end try...catch
-        });
-        
-
-        
-    } // end if
+                catch (e) {
+                    log("Failed to forward the new SMS message.");
+                    log(e);
+                } // end try...catch
+            });
+        } // end if...else
+    }
+    else {
+        log("No TG user ID configured.")
+    } // end if...else
 } // end function forwardSms
 
 /**
@@ -61,7 +87,7 @@ function forwardSms(msg) {
  */
 function readSms() {
     var cursor = context.getContentResolver().query(
-        Uri.parse(java.lang.String("content://sms/inbox")),
+        Uri.parse("content://sms/inbox"),
         null,
         null,
         null,
@@ -72,13 +98,13 @@ function readSms() {
         cursor.moveToFirst();
 
         var msg = util.format(
-            "From: %s\n%s",
+            "<b>From: %s</b>\%0a%s",
             cursor.getString(cursor.getColumnIndex("address")),
             cursor.getString(cursor.getColumnIndex("body"))
-        )
+        );
         
         cursor.close();
-        log("New SMS message read.")
+        log("New SMS message read.");
         return msg;
     }
     else {
@@ -87,18 +113,71 @@ function readSms() {
 } // end function readSms
 
 /**
+ * Show the button log for showing/hiding logs accordingly.
+ */
+function showBtnLog() {
+    if (ui.btnLog.getVisibility() === GONE) {
+        ui.btnLog.setVisibility(VISIBLE);
+        toast("Dev mode enabled.");
+    }
+    else {
+        ui.btnLog.setVisibility(GONE);
+        toast("Dev mode disabled.");
+    } // end if...else
+} // end function showBtnLog
+
+/**
+ * Show logs.
+ */
+function showLogs() {
+    if (!$floaty.checkPermission()) {
+        log("Failed to show a floating window due to no permission. Ask for the permission.");
+        $floaty.requestPermission();
+        exit();
+    }
+    else {
+        if (ui.btnLog.getText() === SHOW_LOGS) {
+            console.show();
+            ui.btnLog.setText(HIDE_LOGS);
+        }
+        else {
+            console.hide();
+            ui.btnLog.setText(SHOW_LOGS);
+        } // end if...else
+    } // end if...else
+} // end function showLogs
+
+/**
  * Start the SMS observer.
  */
 function startObserver() {
+    if (!$power_manager.isIgnoringBatteryOptimizations()) {
+        log("Battery optimisation enabled. Ask for disabling it.");
+        toast("Please disable the battery optimisation on the app to ensure the service.");
+        $power_manager.requestIgnoreBatteryOptimizations();
+    } // end if
+    
     smsObserver = new JavaAdapter(
         ContentObserver,
         {
-            onChange: () => {
+            // It is expected to only react to SMS data changes from [content://sms/raw] (e.g., [content://sms/101]).
+            onChange: (_, uri) => {
+                uri = uri.toString();
+                log("SMS data changes detected from URI: " + uri);
+
+                // Avoid repeated reaction to the SMS data changes. "[]" is necessary.
+                if (uri === "[content://sms]" || uri === "[content://sms/inbox]") {
+                    return;
+                } // end if
+                
                 var msg = readSms();
 
-                if (msg !== null) {
+                if (msg !== null && CONFIG.hasOwnProperty("tgBotToken")) {
                     forwardSms(msg);
-                } // end if
+                }
+                else {
+                    log("Failed to start to forward the new SMS message. Null message or unconfigured TG bot token.")
+                } // end if...else
             }
         },
         new android.os.Handler()
@@ -108,9 +187,17 @@ function startObserver() {
         true,
         smsObserver
     );
-    log("SMS observer started.")
+    log("SMS observer started.");
 } // end function startObserver
 
+$settings.setEnabled("foreground_service", true); // Enable the foreground service to keep the app alive.
+
 ui.layoutFile("res/layout/home.xml");
-ui.run(startObserver); // TODO: check permission and internet connection?
+ui.toolbarApp.on("long_click", showBtnLog);
+ui.txtTg.setText("3. Please add the Telegram (TG) bot named \"" + CONFIG.tgBotName + "\" (" + CONFIG.tgBotLink + "). You should also click the gear button at the bottom to enter your TG user ID to enable TG message forwarding. You can search on the web for the way to get your user ID.")
+ui.btnLog.setText(SHOW_LOGS);
+ui.btnLog.on("click", showLogs);
+ui.fabSettings.on("click", changeSettings);
+ui.run(startObserver);
+
 events.on("exit", endObserver);
